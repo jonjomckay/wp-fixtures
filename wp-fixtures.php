bf5dd40ad77e82d9d55f3bfa1cc766915ddf342e
Jonjo McKay <jonjo@xenolope.com>
2011-04-16 22:44:58 +0100

    Added base plugin info

---
+++ wp-fixtures.php
@@ -0,0 +1,10 @@
+<?php
+	/*
+	Plugin Name: WP Fixtures
+	Plugin URI: https://github.com/jonjomckay/wp-fixtures
+	Description: Plugin for storing sports fixtures in Wordpress, originally designed for netball
+	Author: Jonjo McKay
+	Version: 1.0
+	Author URI: http://www.jonjomckay.com
+	*/
+?>
