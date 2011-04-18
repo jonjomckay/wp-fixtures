1096694447ce0429b81b5600b70341c3408ce8a6
Jonjo McKay <jonjo@xenolope.com>
2011-04-18 20:51:30 +0100

    Took add_menu_page out of function

--- wp-fixtures.php
+++ wp-fixtures.php
@@ -64,9 +64,7 @@
 		add_option("wp_fixtures_db_version", $wp_fixtures_db_version);
 	}
 	
-	function wp_fixtures_admin_menu() {
-		add_menu_page('Title', 'Fixtures',4,'an-uniq-string-as-key', 'wp_fixtures_admin_options', 'http://www.example.com/favicon.ico', 1);
-	}
+	add_menu_page('Title', 'Fixtures',3,'an-uniq-string-as-key', 'wp_fixtures_admin_options', 'http://www.example.com/favicon.ico', 1);
 	
 	function wp_fixtures_admin_options() {
 		if (!current_user_can('manage_options'))  {
@@ -77,5 +75,7 @@
 		echo '<p>Here is where the form would go if I actually had options.</p>';
 		echo '</div>';
 	}
+	
+	add_action('admin_menu', 'wp_fixtures_admin_menu');
 	
 ?>
