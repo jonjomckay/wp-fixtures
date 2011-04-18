debe1ea86bf61090f9ebe1897d66df6e2c33e8cd
Jonjo McKay <jonjo@xenolope.com>
2011-04-18 20:40:54 +0100

    Quick admin menu testing

--- wp-fixtures.php
+++ wp-fixtures.php
@@ -62,4 +62,22 @@
 
 		add_option("wp_fixtures_db_version", $wp_fixtures_db_version);
 	}
+	
+	function wp_fixtures_admin_menu() {
+		
+		add_options_page('My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options');
+	}
+	
+	function wp_fixtures_admin_options() {
+		if (!current_user_can('manage_options'))  {
+			wp_die( __('You do not have sufficient permissions to access this page.') );
+		}
+
+		echo '<div class="wrap">';
+		echo '<p>Here is where the form would go if I actually had options.</p>';
+		echo '</div>';
+	}
+
+}
+	
 ?>
