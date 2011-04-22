f4e81c1f2ce44f25a28b30523af38596c6fe41ef
Jonjo McKay <jonjo@xenolope.com>
2011-04-22 19:47:45 +0100

    Added basic foreach into wp-fixtures-teams and globalised wpdb

--- wp-fixtures-teams.php
+++ wp-fixtures-teams.php
@@ -2,8 +2,7 @@
 	<h2>Add Teams</h2>
 	
 	<?php $teams = $wpdb->get_results('SELECT * FROM ' . $wpdb->prefix . 'wpf_teams'); ?>
-	<?php print_r($teams); ?>
-	<?php /*foreach ($teams as $team) : ?>
+	<?php foreach ($teams as $team) : ?>
 		<li><?php echo $team->name; ?></li>
-	<?php endforeach;*/ ?>
+	<?php endforeach; ?>
 </div>
