fd0f7eae36e43e78f0f71b80dd27b2f5aadcde91
Jonjo McKay <jonjo@xenolope.com>
2011-04-22 19:21:37 +0100

    Added upgrade function to DB schema

--- wp-fixtures.php
+++ wp-fixtures.php
@@ -17,11 +17,13 @@
 	function wp_fixtures_install () {
 	   global $wpdb;
 	   global $wp_fixtures_db_version;
-
-	   if($wpdb->get_var("show tables like '" . $wpdb->prefix . 'wpf-fixtures' . "'") != $wpdb->prefix . 'wpf-fixtures') {
+	   
+	   $installed_ver = get_option( "wp_fixtures_db_version" );
+	   
+	   if( $installed_ver != $wp_fixtures_db_version ) {
 		  
 			$sql = "CREATE TABLE `" . $wpdb->prefix . "wpf-fixtures` (
-			  `id` INT NOT NULL AUTO_INCREMENT,
+			  `id` INT NOT NULL,
 			  `team` INT NULL,
 			  `date` DATETIME NULL,
 			  PRIMARY KEY  (`id`)
@@ -29,12 +31,9 @@
 
 			require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 			dbDelta($sql);
-		}
 		
-		if($wpdb->get_var("show tables like '" . $wpdb->prefix . 'wpf-teams' . "'") != $wpdb->prefix . 'wpf-teams') {
-			
 			$sql = "CREATE TABLE `" . $wpdb->prefix . "wpf-teams` (
-				`id` INT NOT NULL AUTO_INCREMENT,
+				`id` INT NOT NULL,
 				`name` VARCHAR(70) NULL,
 				PRIMARY KEY  (`id`),
 				UNIQUE KEY name_UNIQUE (`name` ASC)
@@ -42,12 +41,9 @@
 
 			require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 			dbDelta($sql);
-		}
 		
-		if($wpdb->get_var("show tables like '" . $wpdb->prefix . 'wpf-venues' . "'") != $wpdb->prefix . 'wpf-venues') {
-			
 			$sql = "CREATE TABLE `" . $wpdb->prefix . "wpf-venues` (
-				`id` INT NOT NULL AUTO_INCREMENT,
+				`id` INT NOT NULL,
 				`name` VARCHAR(70) NULL,
 				`address` VARCHAR(120) NULL,
 				`city` VARCHAR(70) NULL,
@@ -59,9 +55,9 @@
 
 			require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 			dbDelta($sql);
+		
+			update_option("wp_fixtures_db_version", $wp_fixtures_db_version);
 		}
-
-		add_option("wp_fixtures_db_version", $wp_fixtures_db_version);
 	}
 	
 	function wp_fixtures_admin_menu() {
